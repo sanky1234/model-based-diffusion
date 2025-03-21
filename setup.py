28877d06ad2047edd058ec67143fd2a42e1b77ae
Sankalp Yamsani <sankalp.yamsani@gmail.com>
2025-03-20 22:29:27 -0500

    Looks promising"

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='mbd',
     author="Chaoyi Pan",
     author_email="chaoyip@andrew.cmu.edu",
-    packages=find_packages(include="mdb"),
+    packages=find_packages(include=["mbd", "mbd.*"]),
     version='0.0.1',
     install_requires=[
         'gym', 
